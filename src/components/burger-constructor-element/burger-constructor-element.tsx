cea583adf515c282c07d80555df7f0c6a5abe2b0
Roman <romagilda@yandex.ru>
2024-12-03 20:35:47 +0300

    step 8
    (cherry picked from commit 36452db7c44add9e3635959504ba17a049756749)

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -1,14 +1,24 @@
 import { FC, memo } from 'react';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
+import { useDispatch } from '../../services/store';
+import { burgerSlice } from '../../services/reducers/burger-slice';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
-    const handleMoveDown = () => {};
+    const dispatch = useDispatch();
 
-    const handleMoveUp = () => {};
+    const handleMoveDown = () => {
+      dispatch(burgerSlice.actions.moveDownIngredient(index));
+    };
 
-    const handleClose = () => {};
+    const handleMoveUp = () => {
+      dispatch(burgerSlice.actions.moveUpIngredient(index));
+    };
+
+    const handleClose = () => {
+      dispatch(burgerSlice.actions.removeIngredient(index));
+    };
 
     return (
       <BurgerConstructorElementUI
