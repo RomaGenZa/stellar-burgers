b477cfe1c178c39b2bdb7af0f4e81fbae71230e1
Roman <romagilda@yandex.ru>
2024-12-05 01:58:04 +0300

    Fix: IngredientDetails

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -2,19 +2,27 @@ import { FC, useEffect } from 'react';
 import { Preloader } from '@ui';
 import { IngredientDetailsUI } from '@ui';
 import { useParams } from 'react-router-dom';
-import { burgerSlice } from '../../services/reducers/burger-slice';
+import {
+  burgerSlice,
+  fetchIngredients
+} from '../../services/reducers/burger-slice';
 import { useDispatch, useSelector } from '../../services/store';
 
 export const IngredientDetails: FC = () => {
   const params = useParams();
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.burger.ingredients);
 
   useEffect(() => {
     const id = params.id;
     if (id) {
-      dispatch(burgerSlice.actions.updateIngredientsData(id));
+      if (items.length === 0) {
+        dispatch(fetchIngredients());
+      } else {
+        dispatch(burgerSlice.actions.updateIngredientsData(id));
+      }
     }
-  }, [params.id]);
+  }, [params.id, items]);
 
   const ingredientData = useSelector((state) => state.burger.ingredientsData);
 
